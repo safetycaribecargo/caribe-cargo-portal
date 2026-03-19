361c15f29ae27ff100fca3b87f20e242692bffb3
safetycaribecargo <safetycaribecargo@gmail.com>
2026-03-19 15:48:07 -0400

    Add Tailwind CSS configuration for custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+'module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1DA1F2',
+        customOrange: '#FF8C00',
+        customGreen: '#17BF63',
+        customRed: '#E0245E',
+      },
+    },
+  },
+};'
